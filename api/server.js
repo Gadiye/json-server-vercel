4775cf87d817572f87f7ea7faceee921ca3059ae
Gadiye <gadiyek@gmail.com>
2024-07-15 19:29:07 +0300

    chanded2

--- api/server.js
+++ api/server.js
@@ -1,31 +1,36 @@
-// See https://github.com/typicode/json-server#module
 const jsonServer = require('json-server')
+const fs = require('fs')
+const path = require('path')
 
 const server = jsonServer.create()
 
-// Uncomment to allow write operations
-const fs = require('fs')
-const path = require('path')
-const filePath = path.join('db.json')
-const data = fs.readFileSync(filePath, "utf-8");
-const db = JSON.parse(data);
-const router = jsonServer.router(db)
+// Use environment variable for port, default to 3000
+const port = process.env.PORT || 3000
 
-// Comment out to allow write operations
-//const router = jsonServer.router('db.json')
+try {
+    const filePath = path.join(__dirname, 'db.json')
+    const data = fs.readFileSync(filePath, "utf-8");
+    const db = JSON.parse(data);
+    const router = jsonServer.router(db)
 
-const middlewares = jsonServer.defaults()
+    const middlewares = jsonServer.defaults()
 
-server.use(middlewares)
-// Add this before server.use(router)
-server.use(jsonServer.rewriter({
-    '/api/*': '/$1',
-    '/blog/:resource/:id/show': '/:resource/:id'
-}))
-server.use(router)
-server.listen(3000, () => {
-    console.log('JSON Server is running')
-})
+    server.use(middlewares)
+    
+    // Add this before server.use(router)
+    server.use(jsonServer.rewriter({
+        '/api/*': '/$1',
+        '/blog/:resource/:id/show': '/:resource/:id'
+    }))
+    
+    server.use(router)
+    
+    server.listen(port, () => {
+        console.log(`JSON Server is running on port ${port}`)
+    })
+} catch (error) {
+    console.error('Error starting server:', error)
+}
 
 // Export the Server API
 module.exports = server
